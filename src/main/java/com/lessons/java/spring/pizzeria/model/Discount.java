ef16eded72ebf44e890c678fe1809be4b5c2e341
LucaNovelliBoolean <160405407+lucanovellihtml@users.noreply.github.com>
2024-09-17 10:38:07 +0200

    add: create class entity sconto

---
+++ src/main/java/com/lessons/java/spring/pizzeria/model/Discount.java
@@ -0,0 +1,77 @@
+package com.lessons.java.spring.pizzeria.model;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+
+//ENTITA' SCONTO;
+@Entity
+@Table(name = "Discounts")
+public class Discount {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@NotNull
+	@Size(min=2, max=255)
+	private String title;
+		
+	private LocalDateTime startDate;
+	
+	private LocalDateTime finishDate;
+	
+	// 1 - OGGETTO PER STABILIRE LA RELAZIONE CON L'ALTRA ENTITA';
+	// 2 - NOME DELLA COLONNA CHE FUNGERA' DA CHIAVE ESTERNA;
+	private Pizza pizza;
+
+	
+	//GETTER - SETTER
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public LocalDateTime getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(LocalDateTime startDate) {
+		this.startDate = startDate;
+	}
+
+	public LocalDateTime getFinishDate() {
+		return finishDate;
+	}
+
+	public void setFinishDate(LocalDateTime finishDate) {
+		this.finishDate = finishDate;
+	}
+
+	public Pizza getPizza() {
+		return pizza;
+	}
+
+	public void setPizza(Pizza pizza) {
+		this.pizza = pizza;
+	}
+	
+}
